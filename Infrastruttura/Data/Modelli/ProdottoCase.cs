bcb678cb38bc1ff7daf99558977a18a63c999d9f
emalo92 <emanuele.longo992@gmail.com>
2021-08-24 22:49:02 +0200

    Added Model ProdottoCase  + newText

---
+++ Infrastruttura/Data/Modelli/ProdottoCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastruttura.Data.Modelli
+{
+    public class ProdottoCase
+    {
+        public int Codice { get; set; }
+        public int Lotto { get; set; }
+        public int CodiceProgetto { get; set; } //codice del progetto di riferimento.
+        public string NomeProdotto { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Costo { get; set; }
+        public double Peso { get; set; }
+        public int Quantita { get; set; }
+
+        public virtual Progetto CodiceProgettoNavigation { get; set; }
+    }
+}
