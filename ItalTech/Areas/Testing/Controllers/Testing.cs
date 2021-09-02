5e6f618ffe6b867f810ddf6683c5499f4d2f3b3b
Luigi <luigi.maiolino.info@gmail.com>
2021-09-02 20:13:55 +0200

    Added Area Testing

---
+++ ItalTech/Areas/Testing/Controllers/Testing.cs
@@ -0,0 +1,27 @@
+﻿using Infrastruttura;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItalTech.Areas.Testing.Controllers
+{
+    [Area("Testing")]
+    public class Testing : Controller
+    {
+        private readonly ILogger<Testing> _logger;
+        private ITestingDal _testingDal;
+
+        public Testing(ILogger<Testing> logger, ITestingDal testingDal)
+        {
+            _logger = logger;
+            _testingDal = testingDal;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
