5bcdb78ecee8ac1e185390af1ed0a53a3b82e988
emalo92 <emanuele.longo992@gmail.com>
2021-09-08 17:28:33 +0200

    fixxxxxx

--- ItalTech/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ItalTech/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -23,18 +23,15 @@ namespace ItalTech.Areas.Identity.Pages.Account
         private readonly SignInManager<ItalTechUser> _signInManager;
         private readonly UserManager<ItalTechUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
-
+        
         public RegisterModel(
             UserManager<ItalTechUser> userManager,
             SignInManager<ItalTechUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -88,9 +85,6 @@ namespace ItalTech.Areas.Identity.Pages.Account
                         pageHandler: null,
                         values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
